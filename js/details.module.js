765945cccb88e85578d199366d7ee1c52bb4f913
AhmedFayed352 <fayedahmed352@gmail.com>
2024-04-21 05:35:25 +0200

    first commit

---
+++ js/details.module.js
@@ -0,0 +1,35 @@
+import { Ui } from "./ui.module.js";
+
+export class Details {
+  constructor(id) {
+    this.ui = new Ui();
+    document.getElementById("btnClose").addEventListener("click", () => {
+      document.querySelector(".games").classList.remove("d-none");
+      document.querySelector(".details").classList.add("d-none");
+    });
+    this.getDetails(id);
+  }
+  async getDetails(gamesId) {
+    const loading = document.querySelector(".loading");
+    loading.classList.remove("d-none");
+
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "29f773e617msh1d0d33b56cbc9dcp1862bfjsncb4c71dd9d58",
+        "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+      },
+    };
+
+    fetch(
+      `https://free-to-play-games-database.p.rapidapi.com/api/game?id=${gamesId}`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => this.ui.displayDetails(response))
+      .catch((err) => console.error(err))
+      .finally(() => {
+        loading.classList.add("d-none");
+      });
+  }
+}
